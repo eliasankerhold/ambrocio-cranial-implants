5cf361e071f904b6d581994b75d86394a3ca01cd
Elias Ankerhold <elias.ankerhold@gmail.com>
2023-11-07 15:54:40 +0200

    changed formatting

--- src/RhinoFileImporter.py
+++ src/RhinoFileImporter.py
@@ -6,6 +6,7 @@ from datetime import datetime
 
 db_path = os.path.join('C:\\', 'Users', 'ankerhe1', 'Downloads', 'skull_database')
 
+
 def get_filepaths_from_folder(dpath, mask):
     """ 
     Retrieves all files from subdirectories of main folder that end with mask.
@@ -17,19 +18,19 @@ def get_filepaths_from_folder(dpath, mask):
     :return: List of full filepaths.
     :rtype: list[str]
     """
-    
+
     fpaths = []
     for dirpath, dirname, fname in os.walk(dpath):
         cleared_stls = [i for i in fname if i.endswith(mask)]
         if cleared_stls:
             fpaths.append(os.path.join(dirpath, cleared_stls[0]))
-            
-    
+
     print("Found " + str(len(fpaths)) + " files.")
-    
+
     return fpaths
-    
-def reduce_mesh(mesh_id, reduced_polygon_count, accuracy = 4, threaded = True):
+
+
+def reduce_mesh(mesh_id, reduced_polygon_count, accuracy=4, threaded=True):
     """
     Reduces number of polygons of a mesh.
     
@@ -42,7 +43,7 @@ def reduce_mesh(mesh_id, reduced_polygon_count, accuracy = 4, threaded = True):
     :param threaded: Toggles whether reduction should be computed in parallel.
     :param threaded: bool
     """
-    
+
     mesh = rs.coercemesh(mesh_id)
     print("Reducing mesh to " + str(reduced_polygon_count) + " polygons...")
     success = mesh.Reduce(reduced_polygon_count, threaded, accuracy, False, True)
@@ -52,8 +53,9 @@ def reduce_mesh(mesh_id, reduced_polygon_count, accuracy = 4, threaded = True):
         sc.doc.Views.Redraw()
     else:
         print("Unable to reduce mesh")
-    
-def import_files(fpaths, reduced_mesh = 200001):
+
+
+def import_files(fpaths, reduced_mesh=200001):
     """
     Imports all files in fpaths and processes them by reducing their mesh, renamind them and grouping all meshes from one file together.
     
@@ -62,7 +64,7 @@ def import_files(fpaths, reduced_mesh = 200001):
     :param reduced_mesh: Target number of polygons of reduced mesh. 
     :type reduce_mesh: int
     """
-    
+
     start = datetime.now()
     print(str(start) + ": Starting file import.")
     base_cmd = "Import "
@@ -72,19 +74,20 @@ def import_files(fpaths, reduced_mesh = 200001):
         rs.Command(cmd)
         name = os.path.basename(fpath).split(".")[0]
         rs.AddGroup(name)
-        print("File " + str(i+1) + " of " + str(le) + " imported.")
+        print("File " + str(i + 1) + " of " + str(le) + " imported.")
         objs = rs.SelectedObjects()
         print("Found " + str(len(objs)) + " meshes.")
         for j, obj_id in enumerate(objs):
             reduce_mesh(obj_id, reduced_mesh)
             rs.ObjectName(obj_id, name + "_" + str(j))
-        
+
         rs.AddObjectsToGroup(objs, name)
-        rs.UnselectAllObjects()            
-        print("File " + str(i+1) + " of " + str(le) + " processed.")
-        
+        rs.UnselectAllObjects()
+        print("File " + str(i + 1) + " of " + str(le) + " processed.")
+
     print(str(datetime.now()) + ": File import and processing finished.")
     print("Total duration: " + str(datetime.now() - start))
+
 
 fpaths = get_filepaths_from_folder(db_path, '_clear.stl')
 import_files(fpaths)
