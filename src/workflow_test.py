9e45c66514b880d6dff69db22f91314f82c28d2b
Elias Ankerhold <elias.ankerhold@gmail.com>
2023-11-07 23:49:27 +0200

    tested opening, convex hull generation, saving

--- src/workflow_test.py
+++ src/workflow_test.py
@@ -1,26 +1,29 @@
 from file_importer import FileImporter
+from geometry_processor import GeometryProcessor
 
 import os
+from datetime import datetime
 
 importer = FileImporter(import_dir=os.path.join('C:\\', 'Users', 'Elias', 'Downloads', 'A0036-A0040'),
                         export_dir=f'..{os.path.sep}exports')
 
 importer.scan_import_export_structure(mask='_clear.stl')
-print(importer.import_fpaths)
-print(importer.export_fpaths)
 importer.prepare_export_directories()
 
-# def import_and_process(filepath: str):
-#     ms = pml.MeshSet()
-#     ms.load_new_mesh(filepath)
-#     ms.generate_convex_hull()
-#     ms.save_current_mesh('test.stl')
-#
-#
-# db_path = os.path.join('C:\\', 'Users', 'Elias', 'Downloads', 'A0036-A0040')
-#
-# fpaths = get_filepaths_from_folder(db_path, '_clear.stl')
-# import_and_process(fpaths[1])
+processor = GeometryProcessor(file_importer=importer)
+
+start = datetime.now()
+
+for import_path, export_path in zip(importer.import_fpaths.values(), importer.export_fpaths.values()):
+    print(f"Processing {import_path}...")
+    ms = processor.load_file(fpath=import_path)
+    hull = processor.get_convex_hull(ms)
+    processor.save_mesh(mesh=hull, fpath=export_path)
+    print(f"Saved to {export_path}\n")
+
+print(datetime.now() - start)
+print('Done!')
+
 #
 # mesh = o3d.io.read_triangle_mesh('test.stl')
 # mobb = mesh.get_oriented_bounding_box()
