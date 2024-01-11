0b3b5863fb07a9ae0c8b636a5462e7747fb29149
TheBlueCookie <elias.ankerhold@gmail.com>
2024-01-11 19:52:36 +0200

    fixing faulty hover info in online version

--- examples/plotting.py
+++ examples/plotting.py
@@ -3,7 +3,7 @@ from cranalyzer.visualizer import CranialPlot
 
 hits_path = os.path.join('exports', 'MUG500_DB_only_left_hits.csv')
 areas_path = os.path.join('exports', 'MUG500_DB_only_left_areas.csv')
-export_path = os.path.join('exports', 'heatmap_db_mug500_left.html')
+export_path = os.path.join('exports', 'heatmap_db_mug500_left_normal.html')
 
 plotter = CranialPlot(hits_path=hits_path, areas_path=areas_path)
 
